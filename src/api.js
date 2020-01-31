d04ea4ab84107e00a5e34804827b0b663ab864ac
Jose Alvarez <joseche0.ja@gmail.com>
2020-01-31 01:21:33 -0500

    prueba en public

--- src/api.js
+++ src/api.js
@@ -1,18 +1,19 @@
-const BASE_URL = "http://localhost:3001";
+// const BASE_URL = "http://localhost:3001";
+const BASE_URL = "http://polar-inlet-87028.herokuapp.com";
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-const randomNumber = (min = 0, max = 1) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
-const simulateNetworkLatency = (min = 30, max = 1500) =>
-  delay(randomNumber(min, max));
+// const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+// const randomNumber = (min = 0, max = 1) =>
+//   Math.floor(Math.random() * (max - min + 1)) + min;
+// const simulateNetworkLatency = (min = 30, max = 1500) =>
+//   delay(randomNumber(min, max));
 
 async function callApi(endpoint, options = {}) {
-  await simulateNetworkLatency();
+  // await simulateNetworkLatency();
 
-  options.headers = {
-    "Content-Type": "application/json",
-    Accept: "application/json"
-  };
+  // options.headers = {
+  //   "Content-Type": "application/json",
+  //   Accept: "application/json"
+  // };
 
   const url = BASE_URL + endpoint;
   const response = await fetch(url, options);
@@ -22,7 +23,7 @@ async function callApi(endpoint, options = {}) {
 }
 
 const api = {
-  badges: {
+  
     list() {
       return callApi("/badges");
     },
@@ -47,7 +48,7 @@ const api = {
         method: "DELETE"
       });
     }
-  }
+  
 };
 
 export default api;
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -16,18 +16,18 @@ class Badges extends React.Component {
 
   componentDidMount() {
     this.fetchData();
-    this.intervalId = setInterval(this.fetchData, 5000);
+    // this.intervalId = setInterval(this.fetchData, 5000);
   }
 
   componentWillUnmount() {
-    clearInterval(this.intervalId);
+    // clearInterval(this.intervalId);
   }
 
   fetchData = async () => {
     this.setState({ loading: true, error: null });
 
     try {
-      const data = await api.badges.list();
+      const data = await api.list();
       this.setState({ loading: false, data: data });
     } catch (error) {
       this.setState({ loading: false, error: error });
